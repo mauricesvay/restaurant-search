bc1acfc1f37b56c05d37e401effc86463005f3fd
Maurice Svay <maurice@svay.com>
2018-07-15 14:59:58 +0200

    Adds basic search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import algoliasearch from 'algoliasearch';
+import algoliasearchHelper from 'algoliasearch-helper';
 import './App.css';
 
+const APPLICATION_ID = 'GSSRJE9CVZ';
+const API_KEY = '615e7854eb8c66e0ead01ae02add074f';
+const INDEX_NAME = 'dev_restaurant_search';
+var client = algoliasearch(APPLICATION_ID, API_KEY);
+var helper = algoliasearchHelper(client, INDEX_NAME);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      q: '',
+      results: [],
+    };
+
+    helper.on('result', (content) => {
+      this.setState({
+        results: content.hits,
+      });
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({q: event.target.value}, () => {
+      helper.setQuery(this.state.q).search();
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="Search__q">Search</label>
+          <input
+            autoFocus
+            placeholder="e.g: pizza, burger, sushi, café"
+            type="search"
+            name="q"
+            id="Search_q"
+            onKeyUp={this.handleChange}
+            autoComplete="off"
+          />
+        </form>
+        <div className="result">
+          {this.state.results.map((result) => {
+            return <div key={result.objectID}>{result.name}</div>;
+          })}
+        </div>
       </div>
     );
   }
